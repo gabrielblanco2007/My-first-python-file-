f578a646901081779e9ed98b6dcfd44aa8193cfb
Gabriel Tarazona Blanco <121984920+gabrielblanco2007@users.noreply.github.com>
2023-01-05 15:33:52 +0000

    how to make a sentence with a line break

--- strings.py
+++ strings.py
@@ -11,3 +11,7 @@ print(len(my_other_string))
 ##plus + plus math##
 
 print(my_string + " " + my_other_string)
+
+#line break method 
+my_new_line_string = "This is a string\nwith a line break"
+print(my_new_line_string)

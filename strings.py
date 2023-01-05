468570270d08cbf56720d30cd16ecdcb0d19efbe
Gabriel Tarazona Blanco <121984920+gabrielblanco2007@users.noreply.github.com>
2023-01-05 15:27:02 +0000

    add count and first math operation

--- strings.py
+++ strings.py
@@ -2,3 +2,12 @@
 
 my_string = "Mi String"
 my_other_string = "My other String"
+
+## count ##
+
+print(len(my_string))
+print(len(my_other_string))
+
+##plus + plus math##
+
+print(my_string + " " + my_other_string)

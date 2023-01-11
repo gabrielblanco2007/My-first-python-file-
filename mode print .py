9a3a6df565ad2ff4f4ce2b2d94793d97eb77a572
gabrielblanco2007 <gabrieltarazonablanco@gmail.com>
2023-01-11 16:53:53 -0500

    testing conexion with github repo

--- mode print .py
+++ mode print .py
@@ -1,8 +1,9 @@
 #Variable print and type of values
 
-print("First line d")
+print("First line ")
 print(type("Second Line")) # type ' str'
 print(type(5)) # type 'int'
 print(type(1.5)) # type 'float'
 print(type(1 + 2j)) # type 'complex number'
 print(type(True)) # type 'bool'
+print(type(1-1-1--1))
